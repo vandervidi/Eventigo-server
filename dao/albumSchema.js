5f605889507d2065b25a3cd5e50cda4304e905df
Vidran Abdovich <vandervidi@gmail.com>
2015-11-11 22:14:17 +0200

    testing some bug

--- dao/albumSchema.js
+++ dao/albumSchema.js
@@ -8,25 +8,41 @@ var Schema = mongoose.Schema;
 var albumSchema = new Schema({
 
 	shortId: {type: String, unique: true, default: shortid.generate()},
+
 	activeStatus: {type: Boolean, default: true},
+
 	creationDate: {type: Date, default: Date.now},
+
 	creator: {type: Number, ref:'userM' , required :true},  // This is the owner's user ID
 
 	name: {type: String, required :true, default: 'ALBUM NAME'},
+
 	location: {type: String, default: ''},
+
 	coverPhoto: {type: String, default: ''},
 	
+
+
 	photos:[{
-		owner: {type: Number, ref:'userM' , default: -1},  // This is the owner's user ID
-		url: {type: String, default: ''},
-		creationDate: {type: Date, default: Date.now},
-		rating: {
-			positive: [{ type: Number, ref:'userM'}] // Array of users ID's
-		},
-		comments: [{type: Schema.Types.ObjectId, ref: 'commentM'}]
+
+			owner: {type: Number, ref:'userM' , default: -1},  // This is the owner's user ID
+
+			url: {type: String, default: ''},
+
+			creationDate: {type: Date, default: Date.now},
+
+			rating: {
+				positive: [{ type: Number, ref:'userM'}] // Array of users ID's
+			},
+
+
+			comments: [{type: Schema.Types.ObjectId, ref: 'commentM'}]
+
 	}],
 
+
 	participants: [{ type: Number, ref:'userM'}] 	// Array of users ID's that 
+	
 }, {collection: 'albums'});
 
 exports.albumSchema = albumSchema;
--- dao/userSchema.js
+++ dao/userSchema.js
@@ -4,12 +4,18 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema({
 
 	//	index: true => Tells mongo to index this paramater because it is used frequently. It makes querying faster
-	_id: {type:Number, unique: true},  // Facebook userID
+	_id: {type: Number, unique: true},  // Facebook userID
+
 	name: {type: String, default: '', required: true}, // user's Facebook name
+
 	profilePicture: {type: String, required: true},  // Facebook user profile picture URL
+
 	registrationDate: { type: Date, default: Date.now },  // a timestamp, default: current time
+
 	regId: {type: String, default: '-1', required: true }, // Android device registration ID
+
 	albums: [{ type: Schema.Types.ObjectId, ref: 'albumM' , default: []}]	// List of album ID's the user has participated in
+
 }, {collection: 'users'});
 
 exports.userSchema = userSchema;
