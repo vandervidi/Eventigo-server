ec2e0a0152170a3448c22a6ea8ff3b8834c78f81
Vidran Abdovich <vandervidi@gmail.com>
2015-11-12 16:32:13 +0200

    made joinAlbum method code more maintainable and readable

--- modules/usersManager/index.js
+++ modules/usersManager/index.js
@@ -69,45 +69,89 @@ We recieve a short code representing an album.
 @param {object} - http response object
 */
 exports.joinAlbum = function(req, res){
+	//	Find the album that the user wants to join
+	var promise = Album.findOne().where({'shortId': req.body.shortId}).exec();
+
+	//	Add the user ID to album's participants array
+	promise.then(function(albumDoc){
+		//	Check if the user already is a participant in this album...
+		if(albumDoc.participants.indexOf(req.body.userId) > -1){
+			albumDoc.participants.push(req.body.userId);
+		}else{
+			throw new Error("ERROR: User with id " + req.body.userId + " is already a participant in this album");
+		}
+		return albumDoc.save();
+	})
+
+	//	Find the user that wants to join the album
+	.then(function(){
+		return User.findById(req.body.userId).exec();
+	})
+
+	// Save the album ID in the user's albums array
+	.then(function(userDoc){
+		userDoc.albums.unshift(albumDoc._id);
+		return userDoc.save();
+	})
+
+	//	Return a response
+	.then(function(){
+		res.status(200).json({success: true});
+	})
+
+	//	Error handler
+	.catch(function(err){
+		console.log(err);
+		res.status(200).json({success: false});
+	});
+
+
 
 	//	Get the album ID.
-	Album.findOne().where({'shortId': req.body.shortId}).exec(function(err, albumDoc){
-		if(!err){
-			console.log("######### JOIN ALBUM ########")
-			//	Add the user to the album participants array
-			albumDoc.participants.push(req.body.userId);
-			//	save changes
-			albumDoc.save(function(err, newAlbumdoc){
-				if(!err){
+	//Album.findOne().where({'shortId': req.body.shortId}).exec(function(err, albumDoc){
+		// if(!err){
+		// 	console.log("######### JOIN ALBUM ########")
+		// 	//	Add the user to the album participants array
+		// 	//	Before that, first check if he already is a participant
 
-						// Save the album ID in the user's albums array
-						User.findById(req.body.userId).exec(function(err, userDoc){
-							//	Push to albums array
-							userDoc.albums.unshift(albumDoc._id);
-							//	Save
-							userDoc.save(function(err, modifiedUserDoc){
-								if(!err){
-									res.status(200).json({success: true});
-								}else{
-									console.log('There was an error saving the user document');
-									res.status(200).json({success: false});
-								}
-							});
-						});
+		// 	if(albumDoc.participants.indexOf(req.body.userId) > -1){
+		// 		albumDoc.participants.push(req.body.userId);
+		// 	}else{
+		// 		res.status(200).json({success: true});
+		// 	}
 
-				}else{
-					console.log('There was an error adding the user to albums participants array and saving changes');
-					res.status(200).json({success: false});
-				}
-			});
+		// 	//	save changes
+		// 	albumDoc.save(function(err, newAlbumdoc){
+		// 		if(!err){
+
+		// 				// Save the album ID in the user's albums array
+		// 				User.findById(req.body.userId).exec(function(err, userDoc){
+		// 					//	Push to albums array
+		// 					userDoc.albums.unshift(albumDoc._id);
+		// 					//	Save
+		// 					userDoc.save(function(err, modifiedUserDoc){
+		// 						if(!err){
+		// 							res.status(200).json({success: true});
+		// 						}else{
+		// 							console.log('There was an error saving the user document');
+		// 							res.status(200).json({success: false});
+		// 						}
+		// 					});
+		// 				});
+
+		// 		}else{
+		// 			console.log('There was an error adding the user to albums participants array and saving changes');
+		// 			res.status(200).json({success: false});
+		// 		}
+		// 	});
 
 
-		}else{
-			console.log('There was an error finding the wanted album');
-			res.status(200).json({success: false});
-		}
+		// }else{
+		// 	console.log('There was an error finding the wanted album');
+		// 	res.status(200).json({success: false});
+		// }
 
-	});
+	//});
 
 
 	
