244d26ad3877aa6288945c2eb7dac076a180c8b7
Vidran Abdovich <vandervidi@gmail.com>
2015-11-16 13:58:09 +0200

    changed project name to Eventigo

--- modules/cloudinaryConfig/index.js
+++ modules/cloudinaryConfig/index.js
@@ -1,6 +1,6 @@
 //	This module configures Cloudinary credentials
 exports.config = {
-	cloud_name: 'snapify', 
+	cloud_name: 'eventigo', 
   	api_key: '571845123981617', 
  	api_secret: '7sqypeMplOBuD27M0NVyGgwoSDo' 
 }
